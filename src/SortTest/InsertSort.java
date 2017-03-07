e01f603bec273651a672d8f21763fd54c9ccb8d0
hesiyi <490052981@qq.com>
2017-03-07 20:44:48 +0800

    算法

---
+++ src/SortTest/InsertSort.java
@@ -0,0 +1,14 @@
+package SortTest;
+//插入排序
+public class InsertSort {
+	public static void main(String[] args) {
+		int[] a={1,4,2,6,3,7};
+		int n=a.length;
+		for(int i=0;i<n;i++){
+			for(int j=i+1;j<n;j++){
+				
+			}
+		}
+	}
+
+}
